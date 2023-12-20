189bb68b90370c92cf28c6daa6d35147aa329bb5
Katawut <katawut.u@sabuysolutions.com>
2023-12-20 17:51:30 +0700

    Add SEO Meta with React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,8 @@ module.exports = {
     siteUrl: 'https://yourdomain.tld',
     title: 'Intro to Gatsby',
     description: 'Intro to Gatsby project',
-    image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
-  }
-}
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
