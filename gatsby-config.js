3505f4449cae33b715ad4071f0690f0107af0dba
Katawut <katawut.u@sabuysolutions.com>
2023-12-20 16:08:53 +0700

    Add & Querying Site Metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://yourdomain.tld',
+    title: 'Intro to Gatsby',
+    description: 'Intro to Gatsby project',
+    image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+  }
+}
